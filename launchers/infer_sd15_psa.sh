7acee951e686f490be1e5ad5f8cf59e208676948
Torpedo <92668045+Torpedo2648@users.noreply.github.com>
2025-08-01 18:24:28 +0800

    del: codes on master branch

--- launchers/infer_sd15_psa.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch infer.py \
-    --sd_model stable-diffusion-v1-5/stable-diffusion-v1-5 \
-    --meta_data ./data/sage/val/metadata.jsonl \
-    --save_path ./infer_results/sd15-psa --batch_size 1 \
-    --load_psa --psa_path ./trained_models/psa-sd15 \
-    --llm_model Qwen/Qwen2.5-7B-Instruct --embeds_folder ./data/embeddings
--- launchers/infer_sd15_psa_debug.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch infer.py \
-    --sd_model stable-diffusion-v1-5/stable-diffusion-v1-5 \
-    --meta_data ./data/sage/val/metadata.jsonl \
-    --save_path ./infer_results/sd15-debug --batch_size 1 \
-    --load_psa --psa_path ./trained_models/psa-sd15-debug \
-    --llm_model Qwen/Qwen2.5-7B-Instruct --embeds_folder ./data/embeddings
